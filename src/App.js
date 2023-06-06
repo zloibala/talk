ccefa27a3e9820ac634ab1f39320dbc6a229909e
zloibala <boronova25@gmail.com>
2023-06-05 20:50:04 -0500

    !!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import "./App.css";
-
+import AcUnitIcon from "@mui/icons-material/AcUnit";
 function App() {
-  return <div className="App">hello</div>;
+  const [dogImg, setDogImg] = useState("");
+
+  const onClickHandler = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => setDogImg(data.message));
+  };
+
+  return (
+    <div className="App">
+      <h1> Talking to the Moon</h1>
+      <button onClick={onClickHandler}> Get the dog image</button>
+      <div>{dogImg && <img src={dogImg} alt="dog" width="400px" />}</div>
+    </div>
+  );
 }
 
 export default App;
